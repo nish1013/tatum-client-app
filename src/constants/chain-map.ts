9c84e80448a94ae435abcaa4b4ace7324e294acb
nish1013 <nishand2d@gmail.com>
2025-02-14 20:18:31 +0000

    Update chain network constants to support only Ethereum and Ethereum Sepolia

--- src/constants/chain-map.ts
+++ src/constants/chain-map.ts
@@ -1,9 +1,8 @@
 import { Network } from '@tatumio/tatum';
 
 // TODO: Add more networks and chains as needed
+// currently only Ethereum and Ethereum Sepolia are supported
+// due to the api keys limitation
 export const CHAIN_NETWORKS = {
   Ethereum: [Network.ETHEREUM, Network.ETHEREUM_SEPOLIA],
-  Polygon: [Network.POLYGON, Network.POLYGON_AMOY],
-  Bitcoin: [Network.BITCOIN],
-  Solana: [Network.SOLANA],
 };
