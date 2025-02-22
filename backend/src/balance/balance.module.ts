deba04dbe5dbfb79574efa917db6513c21892e19
nish1013 <nishand2d@gmail.com>
2025-02-22 19:11:13 +0000

    feat: add TatumService to BalanceModule providers

--- backend/src/balance/balance.module.ts
+++ backend/src/balance/balance.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BalanceController } from './balance.controller';
 import { BalanceService } from './balance.service';
+import { TatumService } from '../integrations';
 
 @Module({
   controllers: [BalanceController],
-  providers: [BalanceService],
+  providers: [BalanceService, TatumService],
 })
 export class BalanceModule {}
