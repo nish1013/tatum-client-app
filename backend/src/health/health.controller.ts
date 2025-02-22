51ff47598f0a23e1e2e9a11b96da476d7bd522db
nish1013 <nishand2d@gmail.com>
2025-02-22 19:11:18 +0000

    feat: update health controller to include versioning and modify health check comments

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -2,22 +2,26 @@ import { Controller, Get } from '@nestjs/common';
 import {
   HealthCheck,
   HealthCheckService,
-  HttpHealthIndicator,
+  //HttpHealthIndicator,
 } from '@nestjs/terminus';
 
-@Controller('health')
+@Controller({
+  path: 'health',
+  version: '1',
+})
 export class HealthController {
   constructor(
     private readonly healthService: HealthCheckService,
-    private readonly httpHealthIndicator: HttpHealthIndicator,
+    //private readonly httpHealthIndicator: HttpHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
     return this.healthService.check([
-      // TODO: Add taum health check
-      () => this.httpHealthIndicator.pingCheck('google', 'https://google.com'),
+      // TODO: Add taum health check when moving to production with paid plan
+      // need verification of the health check endpoint
+      // () => this.httpHealthIndicator.pingCheck('tatum', 'https://taum.io/v4/health'),
     ]);
   }
 }
