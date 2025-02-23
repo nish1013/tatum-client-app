0327729c92c88c96d71778f92f1f0aa011ef36ae
nish1013 <nishand2d@gmail.com>
2025-02-23 20:23:12 +0000

    feat: remove unused BlockchainChain import from blockchain.service.ts

--- backend/src/core/blockchain/services/blockchain.service.ts
+++ backend/src/core/blockchain/services/blockchain.service.ts
@@ -1,5 +1,4 @@
 import { BlockchainBalance } from '../interfaces/blockchain.balance';
-import { BlockchainChain } from '../interfaces/blockchain.chain';
 import { BlockchainNetwork } from '../interfaces/blockchain.network';
 
 export interface BlockchainService {
