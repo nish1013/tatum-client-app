5a3e66222824680f1c6d72163c95b26db3e70a89
nish1013 <nishand2d@gmail.com>
2025-02-24 08:46:40 +0000

    refactor: replace console.log with Logger for improved logging in BlockchainServiceFactory

--- backend/src/core/blockchain/blockchain.service.factory.ts
+++ backend/src/core/blockchain/blockchain.service.factory.ts
@@ -1,16 +1,19 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Logger } from '@nestjs/common';
 import { TatumService } from '../../integrations';
 import { BlockchainProvider } from './blockchain.provider';
 import { BlockchainService } from './services/blockchain.service';
 
 @Injectable()
 export class BlockchainServiceFactory {
+  private readonly logger = new Logger(BlockchainServiceFactory.name);
+
   constructor(private readonly tatumService: TatumService) {}
 
   public create(provider?: BlockchainProvider): BlockchainService {
     const selectedProvider =
       provider || process.env.BLOCKCHAIN_PROVIDER || BlockchainProvider.TATUM;
-    console.log(`Selected blockchain provider: ${selectedProvider}`);
+
+    this.logger.log(`Selected blockchain provider: ${selectedProvider}`);
 
     switch (selectedProvider) {
       case BlockchainProvider.TATUM:
